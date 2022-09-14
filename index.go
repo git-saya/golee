a13260ff3c9adc6e80e8a9bb166b5e99be90e78f
git-saya <eki.ridyanto@gmail.com>
2022-09-14 11:10:42 +0700

    find a character index.

--- index.go
+++ index.go
@@ -6,13 +6,16 @@ import (
 
 func CensorEmail(email string) string {
 	var str_split = strings.Split(email, "@")
+	initial := str_split[0][0:1] //ambil huruf pertama
 
-	length := len(str_split[0])
+	length := len(str_split[0][1:]) //hitung jumlah string mulai dari index 1: kecuali huruf pertama
+
 	var censor_email string
-	for i := 0; i < length; i++ {
+	for i := 1; i < length; i++ {
 		censor_email += "x" 
 	}
 
-	result := censor_email + "@" + str_split[1]
+	// gabungkan semua variabel
+	result := initial + censor_email + "@" + str_split[1]
 	return result
 }
