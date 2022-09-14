3071980504b5b061e4ddcc377434e1301673112e
git-saya <eki.ridyanto@gmail.com>
2022-09-14 10:22:17 +0700

    len string

--- index.go
+++ index.go
@@ -1,14 +1,24 @@
-package golee
+package main
 
 import (
 	"strings"
+	"fmt"
 )
 
 
 func CensorEmail(email string) string {
 	var str_split = strings.Split(email, "@")
-	censor_email := "xxxxxxx@" 
 
-	result := censor_email + str_split[1]
+	length := len(str_split[0])
+	var censor_email string
+	for i := 0; i < length; i++ {
+		censor_email += "x" 
+	}
+
+	result := censor_email + "@" + str_split[1]
 	return result
+}
+
+func main() {
+	fmt.Println(CensorEmail("bale@gmail.com"))
 }
